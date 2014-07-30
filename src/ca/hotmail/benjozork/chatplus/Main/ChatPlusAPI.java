be0799aba876a4e930fb03984edd73c7ae199c5c
benjozork <benjozork@hotmail.ca>
2014-07-30 10:50:54 -0400

    Update ChatPlusAPI.java

--- src/ca/hotmail/benjozork/chatplus/Main/ChatPlusAPI.java
+++ src/ca/hotmail/benjozork/chatplus/Main/ChatPlusAPI.java
@@ -4,7 +4,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 /**
- *
  * @author Benjozork
  */
 public class ChatPlusAPI {
@@ -17,32 +16,32 @@ public class ChatPlusAPI {
         return ChatPlus.getInstance().nocaps;
     }
 
-    public void setPaused(boolean value) {
-        ChatPlus.getInstance().paused = value;
-    }
-
     public void setLowerCasingEnabled(boolean value) {
         ChatPlus.getInstance().nocaps = value;
     }
 
-    public void setJoinMessage(String msg) {
-        ChatPlus.getInstance().getConfig().set("messages.join", msg);
-    }
-
-    public void setLeaveMessage(String msg) {
-        ChatPlus.getInstance().getConfig().set("messages.leave", msg);
+    public void setPaused(boolean value) {
+        ChatPlus.getInstance().paused = value;
     }
 
     public String getJoinMessage() {
         return ChatPlus.getInstance().getConfig().getString("messages.join");
     }
 
+    public void setJoinMessage(String msg) {
+        ChatPlus.getInstance().getConfig().set("messages.join", msg);
+    }
+
     public String getLeaveMessage() {
         return ChatPlus.getInstance().getConfig().getString("messages.leave");
     }
 
+    public void setLeaveMessage(String msg) {
+        ChatPlus.getInstance().getConfig().set("messages.leave", msg);
+    }
+
     public void displayHelpMessage(Player sender) {
-        sender.sendMessage(ChatColor.AQUA + "<" + ChatColor.BLUE + "======" + ChatColor.UNDERLINE + ChatColor.YELLOW +  " ChatPlus " + ChatPlus.getInstance().getDescription().getVersion() + " by" + ChatColor.LIGHT_PURPLE + " benjozork " + ChatColor.BLUE + "======" + ChatColor.AQUA + ">");
+        sender.sendMessage(ChatColor.AQUA + "<" + ChatColor.BLUE + "======" + ChatColor.UNDERLINE + ChatColor.YELLOW + " ChatPlus " + ChatPlus.getInstance().getDescription().getVersion() + " by" + ChatColor.LIGHT_PURPLE + " benjozork " + ChatColor.BLUE + "======" + ChatColor.AQUA + ">");
         sender.sendMessage(ChatColor.GOLD + "/cp pause -" + ChatColor.GREEN + " Turns on/off the chat");
         sender.sendMessage(ChatColor.GOLD + "/cp set <parameter> <value> -" + ChatColor.GREEN + " Assigns a value to a parameter");
         sender.sendMessage(ChatColor.GOLD + "/cp help -" + ChatColor.GREEN + " Displays this message");
@@ -56,23 +55,46 @@ public class ChatPlusAPI {
         sender.sendMessage(ChatColor.AQUA + "Please use one of these parameters to set:");
         sender.sendMessage(" - " + ChatColor.GOLD + "joinmsg: " + ChatColor.GREEN + "Player join message");
         sender.sendMessage(" - " + ChatColor.GOLD + "leavemsg: " + ChatColor.GREEN + "Player leave message");
+        sender.sendMessage(" - " + ChatColor.GOLD + "pausemsg: " + ChatColor.GREEN + "Chat pause message");
+        sender.sendMessage(" - " + ChatColor.GOLD + "clearmsg: " + ChatColor.GREEN + "Chat clear message");
+        sender.sendMessage(" - " + ChatColor.GOLD + "nocapsmsg: " + ChatColor.GREEN + "Chat lowercasing message");
     }
 
-    public String processMessageTags(String sourcemsg, Player player) {
-        sourcemsg = sourcemsg.replace("%player%", player.getName());
-        sourcemsg = sourcemsg.replace("%ip%", player.getAddress().toString());
-        sourcemsg = sourcemsg.replace("%displayname%", player.getDisplayName());
-        sourcemsg = sourcemsg.replace("%nbplayers%", (ChatPlus.getInstance().getServer().getOnlinePlayers().size() - 1) + "");
-        sourcemsg = ChatColor.translateAlternateColorCodes('&', sourcemsg);
-        return sourcemsg;
-    }
-
-    public String processMessageTagsWithFakePlayer(String sourcemsg, String player) {
-        sourcemsg = sourcemsg.replace("%player%", player);
-        sourcemsg = sourcemsg.replace("%ip%", ""); 
-        sourcemsg = sourcemsg.replace("%displayname%", player);
-        sourcemsg = sourcemsg.replace("%nbplayers%", ((ChatPlus.getInstance().getServer().getOnlinePlayers().size() +  1) + ""));
-        sourcemsg = ChatColor.translateAlternateColorCodes('&', sourcemsg);
-        return sourcemsg;
+    public String processMessageTags(String sourcemsg, Player player, String playername, String method) {
+        if (method.startsWith("TWO")) {
+            //TODO PvP messages
+        } else if (method.startsWith("ONE") || method.startsWith("FAKE")) {
+            //Replace %player% with the player name
+            if (method.startsWith("FAKE_PLAYER_PROCESS")) {
+                sourcemsg = sourcemsg.replace("%player%,", "playername");
+            }else if (method.startsWith("ONE_PLAYER_PROCESS")) {
+                sourcemsg = sourcemsg.replace("%player%", player.getName());
+            }
+            //Replace %ip% with player's ip
+            if (method.equals("ONE_PLAYER_PROCESS_LEAVE") || method.equals("ONE_PLAYER_PROCESS_JOIN")) {
+                sourcemsg = sourcemsg.replace("%ip%", player.getAddress().toString());
+            } else if (method.equals("FAKE_PLAYER_PROCESS_LEAVE") || method.equals("FAKE_PLAYER_PROCESS_JOIN")) {
+                sourcemsg = sourcemsg.replace("%ip%", "");
+            }
+            //Replace player's displayname with his displayname
+            if (method.equals("ONE_PLAYER_PROCESS_LEAVE") || method.equals("ONE_PLAYER_PROCESS_JOIN")) {
+                sourcemsg = sourcemsg.replace("%displayname%", player.getDisplayName());
+            } else if (method.equals("FAKE_PLAYER_PROCESS_LEAVE") || method.equals("FAKE_PLAYER_PROCESS_JOIN")) {
+                sourcemsg = sourcemsg.replace("%displayname%", playername);
+            }
+            //Replace %nbplayers% by the number of players on the server
+            if (method.equals("ONE_PLAYER_PROCESS_LEAVE")) {
+                sourcemsg = sourcemsg.replace("%nbplayers%", (ChatPlus.getInstance().getServer().getOnlinePlayers().size() - 1) + "");
+            } else if (method.equals("ONE_PLAYER_PROCESS_JOIN")) {
+                sourcemsg = sourcemsg.replace("%nbplayers%", ChatPlus.getInstance().getServer().getOnlinePlayers().size() + "");
+            }
+            //Replace & codes with ChatColors
+            sourcemsg = ChatColor.translateAlternateColorCodes('&', sourcemsg);
+            return sourcemsg;
+        }else {
+            ChatPlus.getInstance().getLogger().severe("ChatPlusAPI: FATAL ERROR: INVALID PROCESSING METHOD !!! ");
+            return "";
+        }
+        return "";
     }
 }
